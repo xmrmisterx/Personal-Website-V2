39e2df8b70d8122ad66558a0299b5053e9ce8aaa
xmrmisterx <spoonman22@gmail.com>
2022-08-14 18:47:06 -0600

    created cleardb Heroku add on and put db info into dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-  host            : 'us-cdbr-east-04.cleardb.com',
-  user            : 'ba78fb274f2846',
-  password        : 'e7c84551',
-  database        : 'heroku_2502c22c3414591',
+  host            : 'us-cdbr-east-06.cleardb.net',
+  user            : 'b71e93b0879965',
+  password        : 'fb2590aa',
+  database        : 'heroku_67e0c751e4b8160',
 });
+
+// var pool = mysql.createPool({
+//   host            : 'us-cdbr-east-04.cleardb.com',
+//   user            : 'ba78fb274f2846',
+//   password        : 'e7c84551',
+//   database        : 'heroku_2502c22c3414591',
+// });
 
 module.exports.pool = pool;
