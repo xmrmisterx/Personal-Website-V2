7cccb0f9b2c4e79b8a74fd3a84559e9fe63632f8
xmrmisterx <spoonman22@gmail.com>
2024-09-12 21:11:25 -0600

    update database credentials

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
+// var pool = mysql.createPool({
+//   host            : 'us-cdbr-east-06.cleardb.net',
+//   user            : 'b71e93b0879965',
+//   password        : 'fb2590aa',
+//   database        : 'heroku_67e0c751e4b8160',
+// });
+
 var pool = mysql.createPool({
-  host            : 'us-cdbr-east-06.cleardb.net',
-  user            : 'b71e93b0879965',
-  password        : 'fb2590aa',
-  database        : 'heroku_67e0c751e4b8160',
+  host            : 'caij57unh724n3.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+  user            : 'ue4ladd0heslkf',
+  password        : 'p885b1ed81a9e330617033050d23165f0a50d441f71d06fa88839040452afb112',
+  database        : 'd9uhqmpgn3to31',
 });
 
 module.exports.pool = pool;
